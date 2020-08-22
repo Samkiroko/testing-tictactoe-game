954a4a5c91a4f060b74303c15a9e57d8ed2b5624
Samkiroko <kirokodev@gmail.com>
2020-08-23 00:25:52 +0300

    clearing linter errors

--- assets/js/gameBoard.js
+++ assets/js/gameBoard.js
@@ -11,7 +11,7 @@ const gameBoard = () => {
 
   const checkDraw = (player, arr = false) => {
     const board2 = arr || board;
-    const positions = board2.filter((x) => x !== '');
+    const positions = board2.filter(x => x !== '');
     if (positions.length === 9) {
       return true;
     }
@@ -20,66 +20,35 @@ const gameBoard = () => {
 
   const checkWin = (player, arr = false) => {
     const board2 = arr || board;
-    if (
-      board2[0] !== ''
-      && board2[0] === board2[1]
-      && board2[1] === board2[2]
-    ) {
+    if (board2[0] !== '' && board2[0] === board2[1] && board2[1] === board2[2]) {
       return true;
     }
-    if (
-      board2[3] !== ''
-      && board2[3] === board2[4]
-      && board2[4] === board2[5]
-    ) {
+    if (board2[3] !== '' && board2[3] === board2[4] && board2[4] === board2[5]) {
       return true;
     }
-    if (
-      board2[6] !== ''
-      && board2[6] === board2[7]
-      && board2[7] === board2[8]
-    ) {
+    if (board2[6] !== '' && board2[6] === board2[7] && board2[7] === board2[8]) {
       return true;
     }
-    if (
-      board2[0] !== ''
-      && board2[0] === board2[3]
-      && board2[3] === board2[6]
-    ) {
+    if (board2[0] !== '' && board2[0] === board2[3] && board2[3] === board2[6]) {
       return true;
     }
-    if (
-      board2[1] !== ''
-      && board2[1] === board2[4]
-      && board2[4] === board2[7]
-    ) {
+    if (board2[1] !== '' && board2[1] === board2[4] && board2[4] === board2[7]) {
       return true;
     }
-    if (
-      board2[2] !== ''
-      && board2[2] === board2[5]
-      && board2[5] === board2[8]
-    ) {
+    if (board2[2] !== '' && board2[2] === board2[5] && board2[5] === board2[8]) {
       return true;
     }
-    if (
-      board2[0] !== ''
-      && board2[0] === board2[4]
-      && board2[4] === board2[8]
-    ) {
+    if (board2[0] !== '' && board2[0] === board2[4] && board2[4] === board2[8]) {
       return true;
     }
-    if (
-      board2[2] !== ''
-      && board2[2] === board2[4]
-      && board2[4] === board2[6]
-    ) {
+    if (board2[2] !== '' && board2[2] === board2[4] && board2[4] === board2[6]) {
       return true;
     }
 
     // return checkDraw(player);
     return false;
   };
+
 
   return {
     board,
